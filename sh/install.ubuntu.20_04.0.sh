354385c69c39e7884d5e03ad9648d016f010611a
leandrosardi <leandro.sardi@expandedventure.com>
2024-03-31 14:52:49 -0300

    automated installation

--- sh/install.ubuntu.20_04.0.sh
+++ sh/install.ubuntu.20_04.0.sh
@@ -17,19 +17,19 @@ fi
 echo "Password: $1"
 
 # add user with password
-useradd -p $(openssl passwd -1 $1) blackstack > ~/0.log 2>&1
+useradd -p $(openssl passwd -1 $1) blackstack >> ~/0.log 2>>&1
 
 # add blackstack to sudoers
-usermod -aG sudo blackstack > ~/0.log 2>&1
+usermod -aG sudo blackstack >> ~/0.log 2>>&1
 
 # change hostname
-hostname mp123 > ~/0.log 2>&1
+hostname mp123 >> ~/0.log 2>>&1
 
 # edit /etc/ssh/sshd_config, enable the line "PasswordAuthentication yes"
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config > ~/0.log 2>&1
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config >> ~/0.log 2>>&1
 
 # restart ssh service
-service ssh restart > ~/0.log 2>&1
+service ssh restart >> ~/0.log 2>>&1
 
 # create home directory for blackstack
 #mkdir /home/blackstack
